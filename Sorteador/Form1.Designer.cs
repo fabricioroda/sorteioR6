3c9d3c6125395ebbdc87c1358c0318405c638348
Fabricio Roda Sales <fsales@FAZENDA.MS>
2019-07-29 12:29:16 -0400

    Projeto novo

---
+++ Sorteador/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Sorteador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxQtde = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(478, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "QUANTIDADE DE NUMEROS A SEREM SORTEADOS";
+            // 
+            // txtBoxQtde
+            // 
+            this.txtBoxQtde.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxQtde.Location = new System.Drawing.Point(49, 61);
+            this.txtBoxQtde.Name = "txtBoxQtde";
+            this.txtBoxQtde.Size = new System.Drawing.Size(131, 26);
+            this.txtBoxQtde.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(230, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 62);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "SORTEIO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.Color.Red;
+            this.lblResultado.Location = new System.Drawing.Point(49, 152);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(79, 29);
+            this.lblResultado.TabIndex = 3;
+            this.lblResultado.Text = "label2";
+            this.lblResultado.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 227);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBoxQtde);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Sorteio R6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxQtde;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Sorteador/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorteador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+                Random randNum = new Random();
+                string qtde1 = txtBoxQtde.Text;
+                int qtde2 = Convert.ToInt16(qtde1);
+                    //MessageBox.Show("Resultado do Sorteio é " + randNum.Next(qtde2).ToString());
+                    lblResultado.Text = "Numero do Membro do R6 Sorteado é " + (randNum.Next(1, qtde2).ToString());
+                    lblResultado.Visible = true;
+              
+
+            
+        }
+    }
+}
